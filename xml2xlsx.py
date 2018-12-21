a0975555075d5bcad659698facb6b8fd83f95816
aznboystride <FairNuri.aboshehwa@student.csulb.edu>
2018-12-21 11:55:04 -0800

    added xml2xlsx to convert xml sheet to xlsx

---
+++ xml2xlsx.py
@@ -0,0 +1,26 @@
+import os
+import xml.etree.ElementTree as et
+import openpyxl
+
+
+def xml2xlsx(xmlfile, savepath):
+	tree = et.parse(xmlfile)
+	root = tree.getroot()
+	rows = root[0][0]
+	wb = openpyxl.load_workbook(savepath)
+	sheet = wb['Sheet 1']
+	sheet.delete_rows(1, 300)
+	r = 1
+	for row in rows:
+		c = 1
+		for cell in row:
+			sheet.cell(row=r, column=c).value = cell[0].text
+			c += 1
+		r += 1
+	wb.save(savepath)
+
+def main():
+	xml2xlsx("/Users/Pey/Documents/redline/recent_invoice.xls", "/Users/Pey/Documents/redline/test.xlsx")
+
+if __name__ == '__main__':
+	main()
